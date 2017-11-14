cb678d154918c8b60885f50821d0e1c458b71120
Mitxel Moriana <mmoriana@cvaconsulting.com>
2017-11-14 11:05:31 +0100

    add renderer, renderable, templates examples

---
+++ classes/inplace_editable.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_gallery;
+
+defined('MOODLE_INTERNAL') || die;
+
+use core\output\inplace_editable as core_inplace_editable;
+use lang_string;
+use renderer_base;
+
+/**
+ * Class mod_gallery_inplace_editable
+ */
+class inplace_editable extends core_inplace_editable {
+    public function __construct($itemtype, $itemid, $value, $editable) {
+        $displayvalue = format_string($value);
+        $hint = new lang_string('inplace_hint', 'mod_gallery');
+        $editlabel = new lang_string('editlabel', 'mod_gallery') . ' ' . format_string($value);
+
+        parent::__construct('mod_gallery', $itemtype, $itemid, $editable, $displayvalue, $value, $hint, $editlabel);
+    }
+
+    /**
+     * Renders this element
+     *
+     * @param renderer_base $output typically, the renderer that's calling this function
+     * @return string
+     */
+    public function render(renderer_base $output) {
+        return $output->render_from_template('mod_newmodule/inplace_editable', $this->export_for_template($output));
+    }
+}
---
+++ classes/output/view_page.php
@@ -0,0 +1,70 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Class containing data for gallery assignment view page
+ *
+ * @package    mod_gallery
+ * @copyright  2017 SM - CV&A Consulting <mmoriana@cvaconsulting.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_newmodule\output;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/webservice/externallib.php');
+require_once(__DIR__ . '/../../locallib.php');
+
+use context;
+use mod_gallery\external\example_exporter;
+use renderable;
+use stdClass;
+use templatable;
+use renderer_base;
+
+/**
+ * Class containing data for index page
+ *
+ * @copyright  2017 SM - CV&A Consulting <mmoriana@cvaconsulting.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class view_page implements renderable, templatable {
+    private $context;
+
+    /**
+     * gallery_assignment_page constructor.
+     *
+     * @param context $context
+     */
+    public function __construct($context) {
+        $this->context = $context;
+    }
+
+    /**
+     * Export this data so it can be used as the context for a mustache template.
+     *
+     * @param renderer_base $output
+     * @return array|stdClass
+     */
+    public function export_for_template(renderer_base $output) {
+        $exporter = new example_exporter(null, [
+            'context' => $this->context,
+        ]);
+
+        return $exporter->export($output);
+    }
+}
